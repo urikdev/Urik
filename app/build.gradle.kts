a78f547b562b5cae51ee905d32f51362da4f496f
urikdev <urikdevelopment@proton.me>
2025-10-23 17:36:42 -0500

    Updates build number for release 0.1.0-beta (#38) (#39)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.12-alpha"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
