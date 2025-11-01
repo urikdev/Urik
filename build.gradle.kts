5c6437c9936bdb190e9757b1b48cf41ed93e5afc
urikdev <urikdevelopment@proton.me>
2025-11-01 13:18:45 -0500

    Updates deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,8 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
+    id("com.google.devtools.ksp") version "2.3.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("androidx.room") version "2.8.3" apply false
