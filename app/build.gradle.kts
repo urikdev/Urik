fb1c07d187aa89507a742d4dae45438c66f00e38
Urik Development <urikdevelopment@proton.me>
2025-11-09 13:43:10 -0600

    Bump version number for internal testing (#152) (#153)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.urik.keyboard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.4.2-beta"
+        versionCode = 13
+        versionName = "0.5.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
