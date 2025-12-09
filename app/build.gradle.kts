85aa7b34a0c5ec83bd64ae8414459bc7672c4736
Urik Development <hello@urik.io>
2025-12-08 20:22:14 -0600

    Bump version number for open beta testing (#307)
    * Bump version number for open beta testing
    
    * Update signing config for CodeQL
    
    * Fix workflow sha pinning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,30 @@ android {
         applicationId = "com.urik.keyboard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "0.10.0-beta"
+        versionCode = 38
+        versionName = "0.10.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            val keystorePath = System.getenv("RELEASE_KEYSTORE_PATH")
+            if (keystorePath != null) {
+                storeFile = file(keystorePath)
+                storePassword = System.getenv("RELEASE_STORE_PASSWORD")
+                keyAlias = System.getenv("RELEASE_KEY_ALIAS")
+                keyPassword = System.getenv("RELEASE_KEY_PASSWORD")
+            }
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            val keystorePath = System.getenv("RELEASE_KEYSTORE_PATH")
+            if (keystorePath != null) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
