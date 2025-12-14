bb1714a60f1ecde82adeddce661da1b746d7eabb
urikdev <hello@urik.io>
2025-12-14 07:13:19 -0600

    Code cleanup, bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "8.13.1" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.3.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("androidx.room") version "2.8.4" apply false
-    id("org.jetbrains.kotlinx.kover") version "0.9.3" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.9.4" apply false
 }
