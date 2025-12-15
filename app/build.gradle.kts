c266985891b183e07aab675f5849a8ed3b943746
urikdev <hello@urik.io>
2025-12-14 19:24:29 -0600

    Bump version number for open beta testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.urik.keyboard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.11.0-beta"
+        versionCode = 41
+        versionName = "0.11.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
