ef4e478e2701b2ac550bba2b3abe695e46402ca6
urikdev <hello@urik.io>
2025-12-26 10:08:18 -0600

    Code cleanup

--- app/src/main/java/com/urik/keyboard/utils/ScriptDetector.kt
+++
@@ -1,9 +0,0 @@
-package com.urik.keyboard.utils
-
-import com.ibm.icu.lang.UScript
-
-object ScriptDetector {
-    fun getScriptFromLocale(): Int = UScript.LATIN
-
-    fun isRtlScript(): Boolean = false
-}
