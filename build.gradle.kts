8e99fe6d96ba94e8b5e80ace2c38048f5adb625a
urikdev <hello@urik.io>
2026-01-04 09:17:02 -0600

    Bumps version number for open beta testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.devtools.ksp") version "2.3.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
